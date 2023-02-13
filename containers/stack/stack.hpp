9eda3f04c8bf84d85ef32d94e6741a3809c5f704
Luna <luna@MacBook-Pro.local>
2023-02-13 19:25:21 +0100

    start working in stack & add tester 2

--- containers/stack/stack.hpp
+++ containers/stack/stack.hpp
@@ -44,26 +44,11 @@ namespace ft
             ~stack() {}
         /* ================================ Element access =============================== */
             bool empty() const { return (c.empty()); }
-            size_type size() const
-            {
-                return (c.size());
-            }
-            value_type& top()
-            {
-                return (c.back());
-            }
-            const value_type& top() const
-            {
-                return (c.back());
-            }
-            void push (const value_type& val)
-            {
-                c.push_back(val);
-            }
-            void pop()
-            {
-                c.pop_back();
-            }
+            size_type size() const { return (c.size()); }
+            value_type& top() { return (c.back()); }
+            const value_type& top() const { return (c.back()); }
+            void push (const value_type& val) { c.push_back(val); }
+            void pop() { c.pop_back(); }
     };
     template <class T, class Container>
     bool operator== (const stack<T,Container>& lhs, const stack<T,Container>& rhs) { return lhs.c == rhs.c; }
